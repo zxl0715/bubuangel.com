2d07c656a0c51661af3271a592472b756d45b507
zxl0715 <zxl0715@163.com>
2016-10-09 15:44:15 +0800

    http://www.cnblogs.com/mzwhj/p/3538108.html
    三、项目之间的引用
    项目建立好之后要添加项目间的引用。
    
    在web项目的引用上点右键，选择解决方案->项目 添加Ninesky.Models、 Ninesky.Common、 Ninesky.IBLL、 Ninesky.BLL的引用
    
    依次：IDAL添加Ninesky.Models引用
    
    DAL添加Ninesky.Modles、 Ninesky.IDAL引用
    
    IBLL添加Ninesky.Modles引用
    
    BLL添加Ninesky.Models、 Ninesky.IDAL、 Ninesky.DAL、 Ninesky.IBLL引用

---
+++ Zxl.Common/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zxl.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Zxl.IDAL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zxl.IDAL
+{
+    public class Class1
+    {
+    }
+}
